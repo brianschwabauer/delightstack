c3445481af46010cd2937e0e9c4ee6c33bd99aa9
Brian Schwabauer <brian@brianschwabauer.com>
2025-11-19 17:07:36 -0600

    Initial outline for ThemeToggle component

--- packages/components/src/actions/ThemeToggle.svelte
+++ packages/components/src/actions/ThemeToggle.svelte
@@ -1,3 +1,25 @@
-<pre>
-	https://daisyui.com/components/theme-controller/
-</pre>
+<script lang="ts">
+	/**
+	 * A simple theme toggle component that switches between light, dark, auto modes.
+	 * If auto mode is selected, the theme will follow the system preference.
+	 */
+	let {
+		/** Whether the system/auto theme should be disabled as an option */
+		disableAuto = false,
+
+		/** Whether to show the toggle as an icon instead of a select dropdown */
+		icon = false,
+
+		/** The current theme: 'light', 'dark', or 'auto' */
+		theme = $bindable('auto') as 'light' | 'dark' | 'auto',
+
+		/**
+		 * Whether the theme is dark (this can happen when theme is 'auto' and the user has dark mode enabled)
+		 * This starts off as undefined until the component mounts and checks the system preference
+		 */
+		isDark = $bindable(undefined) as boolean | undefined,
+
+		/** Emits the current theme: 'light', 'dark', or 'auto' */
+		onchange = (() => {}) as (theme: 'light' | 'dark' | 'auto') => void,
+	} = $props();
+</script>
