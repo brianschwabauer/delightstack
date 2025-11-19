920420c45b90abb2c7053469a563436801699030
Brian Schwabauer <brian@brianschwabauer.com>
2025-11-19 16:47:48 -0600

    Add Alert component with modal and action buttons

---
+++ packages/components/src/actions/Alert.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Modal from './Modal.svelte';
+	import Button from './Button.svelte';
+
+	let {
+		/** Title text displayed as the dialog header */
+		title = '',
+
+		/** The message text displayed as the dialog body */
+		message = '',
+
+		/** The text for the "Cancel" button */
+		cancelText = 'Cancel',
+
+		/** The text for the "Continue" button */
+		continueText = 'Continue',
+
+		/** Called when the user hits the "Cancel" button */
+		oncancel = (() => {}) as () => void,
+
+		/** Called when the user hits the "Continue" button */
+		oncontinue = (() => {}) as () => void,
+	} = $props();
+	
+</script>
+
+<Modal {title} {oncancel} {oncontinue}>
+	{#if message}
+		<div class="message">
+			{message}
+		</div>
+	{/if}
+	{#snippet footerEnd}
+		<div class="actions">
+			<Button transparent onclick={oncancel}>{cancelText}</Button>
+			<Button onclick={oncontinue}>{continueText}</Button>
+		</div>
+	{/snippet}
+</Modal>
